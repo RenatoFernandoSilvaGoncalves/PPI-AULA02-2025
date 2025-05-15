bc393ed0202093ef850d41ed3542a7ab8a8d4d18
RenatoFernandoSilvaGoncalves <renatogoncalves@unoeste.edu.br>
2025-05-15 20:39:53 -0300

    Conclusão da validação do cadastro de Usuario. Novas opções no menu. Form de Login e opção sair.

--- index.js
+++ index.js
@@ -32,7 +32,16 @@ app.get("/", (requisicao, resposta) => {
                                     </a>
                                     <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
                                         <li><a class="dropdown-item" href="/cadastroUsuario">Cadastro de Usuários</a></li>
+                                        <li><hr class="dropdown-divider"></li>
+                                        <li><a class="dropdown-item" href="#">Cadastro de Fornecedores</a></li>
+                                        <li><hr class="dropdown-divider"></li>
+                                        <li><a class="dropdown-item" href="#">Cadastro de Clientes</a></li>
+                                        <li><hr class="dropdown-divider"></li>
+                                        <li><a class="dropdown-item" href="#">Cadastro de Produtos</a></li>
                                     </ul>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="/logout">Sair</a>
                                 </li>
                             </ul>
                         </div>
@@ -171,27 +180,72 @@ app.post("/cadastroUsuario", (requisicao, resposta) => {
         conteudo = conteudo + `</div>
                             <div class="col-md-4">
                                 <label for="nomeUsuario" class="form-label">Nome do usuário:</label>
-                                <div class="input-group has-validation">
+                                <div class="input-group"> `;
+        if (!nomeUsuario) {
+            conteudo = conteudo + `
                                     <span class="input-group-text" id="inputGroupPrepend">@</span>
-                                    <input type="text" class="form-control" id="nomeUsuario" name="nomeUsuario" value="${nomeUsuario}" aria-describedby="inputGroupPrepend" required>
+                                    <input type="text" class="form-control" id="nomeUsuario" name="nomeUsuario" aria-describedby="inputGroupPrepend" required>
+                                    <span class="text-danger">Por favor informe a credencial nomeUsuario!</span>`;
+        }
+        else {
+            conteudo = conteudo + `          
+                                    <span class="input-group-text" id="inputGroupPrepend">@</span>
+                                    <input type="text" class="form-control" id="nomeUsuario" name="nomeUsuario" value="${nomeUsuario}" aria-describedby="inputGroupPrepend" required>`;
+        }
+        conteudo = conteudo + `
                                 </div>
                             </div>
-                            <div class="col-md-6">
+                            <div class="col-md-6"> `;
+        if (!cidade) {
+            conteudo = conteudo +
+                `<label for="cidade" class="form-label">Cidade</label>
+                                     <input type="text" class="form-control" id="cidade" name="cidade" required>
+                                     <span class="text-danger">Por favor informe a cidade!</span>`;
+        }
+        else {
+            conteudo = conteudo + `
                                 <label for="cidade" class="form-label">Cidade</label>
-                                <input type="text" class="form-control" id="cidade" name="cidade" value="${cidade}" required>
+                                <input type="text" class="form-control" id="cidade" name="cidade" value="${cidade}" required>`;
+        }
+
+        conteudo = conteudo + `
                             </div>
                             <div class="col-md-3">
-                                <label for="uf" class="form-label">UF</label>
+                                <label for="uf" class="form-label">UF</label>`;
+        if (!uf) {
+            conteudo = conteudo + `
                                 <select class="form-select" id="uf" name="uf" required>
                                     <option selected disabled value="">Escolha um estado...</option>
                                     <option value="SP">SP</option>
                                     <option value="RJ">RJ</option>
                                     <option value="PR">PR</option>
+                                </select>`;
+        }
+        else {
+            conteudo = conteudo + `
+                                <select class="form-select" id="uf" name="uf" required>
+                                    <option disabled value="">Escolha um estado...</option>
+                                    <option ${uf == 'SP' ? 'selected' : ''} value="SP">SP</option>
+                                    <option ${uf == 'RJ' ? 'selected' : ''} value="RJ">RJ</option>
+                                    <option ${uf == 'PR' ? 'selected' : ''} value="PR">PR</option>
                                 </select>
+            `;
+        }
+        conteudo = conteudo + `
                             </div>
                             <div class="col-md-3">
-                                <label for="cep" class="form-label">CEP</label>
-                                <input type="text" class="form-control" id="cep" name="cep" value="${cep}" required>
+                                <label for="cep" class="form-label">CEP</label>`;
+        if (!cep) {
+            conteudo = conteudo + `
+                                <input type="text" class="form-control" id="cep" name="cep" required>
+                                <span class="text-danger">Por favor informe o cep!</span>`;
+        }
+        else {
+            conteudo = conteudo + `
+                                <input type="text" class="form-control" id="cep" name="cep" value=${cep} required>
+            `;
+        }
+        conteudo = conteudo + `
                             </div>
                             <div class="col-12">
                                 <button class="btn btn-primary" type="submit">Cadastrar</button>
@@ -251,6 +305,79 @@ app.get("/listaUsuarios", (requisicao, resposta) => {
             </html>`
     resposta.send(conteudo);
     resposta.end();
+});
+
+app.get("/login", (requisicao, resposta) => {
+    resposta.send(`
+    <html lang="pt-br">
+        <head>
+            <meta charset="UTF-8">
+            <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+            <title>Login do Sistema</title>
+            <style>
+                body {
+                    margin: 0;
+                    padding: 0;
+                    background-color: #17a2b8;
+                    height: 100vh;
+                }
+                #login .container #login-row #login-column #login-box {
+                    margin-top: 120px;
+                    max-width: 600px;
+                    height: 320px;
+                    border: 1px solid #9C9C9C;
+                    background-color: #EAEAEA;
+                }
+                #login .container #login-row #login-column #login-box #login-form {
+                    padding: 20px;
+                }
+                #login .container #login-row #login-column #login-box #login-form #register-link {
+                    margin-top: -85px;
+                }
+            </style>
+        </head>
+        <body>
+            <div id="login">
+                <h3 class="text-center text-white pt-5">Acessar o Sistema</h3>
+                <div class="container">
+                    <div id="login-row" class="row justify-content-center align-items-center">
+                        <div id="login-column" class="col-md-6">
+                            <div id="login-box" class="col-md-12">
+                                <form id="login-form" class="form" action="" method="post">
+                                    <h3 class="text-center text-info">Login</h3>
+                                    <div class="form-group">
+                                        <label for="usuario" class="text-info">Usuário:</label><br>
+                                        <input type="text" name="usuari9o" id="usuario" class="form-control">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="senha" class="text-info">Senha:</label><br>
+                                        <input type="text" name="senha" id="senha" class="form-control">
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="submit" name="submit" class="btn btn-info btn-md" value="Entrar">
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </body>
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    </html>
+    `);
+});
+
+app.post("/login", (requisicao, resposta) => {
+    const usuario = requisicao.body.usuario;
+    const senha = requisicao.body.senha;
+    //Realizar validação
+    resposta.redirect("/");
+});
+
+app.get("/logout", (requisicao, resposta) => {
+    resposta.send("<p>Você saiu do sistema.</p>");
 });
 
 app.listen(port, host, () => {
